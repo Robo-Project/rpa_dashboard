f63ee309b39ad4b115268635263091e075367d8a
tumajote <tuukka.mj.terho@helsinki.fi>
2020-02-10 11:00:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker container run --name jenkins --rm --detach --publish 8080:8080 --publish 50000:50000 --group-add $(stat -c '%g' /var/run/docker.sock) -v /var/run/docker.sock:/var/run/docker.sock roboteam/custom-jenkins
+docker container run --name jenkins --rm --detach --publish 8080:8080 --publish 50000:50000 --group-add $(stat -c '%g' /var/run/docker.sock) -v /var/run/docker.sock:/var/run/docker.sock roboteam/custom-jenkins -v jenkins-data:/var/jenkins_home 
 docker-compose up
