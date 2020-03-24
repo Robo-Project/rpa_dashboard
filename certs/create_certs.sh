08381254eaee8a1df298019d89d8dec58d94e945
Pasi Alho <alho.pasi@iki.fi>
2020-03-24 14:28:31 +0200

    fix create_certs.sh

--- certs/create_certs.sh
+++ certs/create_certs.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-openssl req -x509 -newkey rsa:4096 -keyout robo.key -out robo.crt -days 365 -nodes -subj "/C=FI/L=Helsinki/O=Robo-Team/CN=RoboticProcessAutomationDashboard"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+openssl req -x509 -newkey rsa:4096 -keyout $DIR/robo.key -out $DIR/robo.crt -days 365 -nodes -subj "/C=FI/L=Helsinki/O=Robo-Team/CN=RoboticProcessAutomationDashboard"
